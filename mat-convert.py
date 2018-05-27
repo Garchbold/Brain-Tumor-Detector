6976093f3c6a495512a22b921dd5d0c6df77c376
Nicholas Turk <nturk19@ucla.edu>
2018-05-26 20:50:43 -0700

    Added some error checking

--- mat-convert.py
+++ mat-convert.py
@@ -16,7 +16,6 @@ if not os.path.exists(os.path.dirname(newDir)): #make new directory for all of t
 np.set_printoptions(threshold=np.inf)
 
 #print(sorted(os.listdir('braintumors_1')))
-loading_value = 0
 
 for file in sorted(os.listdir('braintumors_1')): #iterate through all .mat files in braintumors_1 directory
 	currMatFileNum = file.split('.')[0] #current file in braintumors_1 directory; splits 3064.mat into ['3064', 'mat']
@@ -26,7 +25,7 @@ for file in sorted(os.listdir('braintumors_1')): #iterate through all .mat files
 	#print("New csv filename: ", csvFileName) #e.g. "3064.csv" from 3064.mat
 
 	try:
-	   saveToFile = open("brain_csvs/{}".format(csvFileName), "w+") #actually open/make new .csv file to write to from each .mat file (file object)
+	   saveToFile = open("brain_csvs/{}".format(csvFileName), "wb") #actually open/make new .csv file to write to from each .mat file (file object)
 	except OSError as e: # race condition guard
 		if e.errno != errno.EEXIST:
 			raise
@@ -34,9 +33,22 @@ for file in sorted(os.listdir('braintumors_1')): #iterate through all .mat files
 			raise e
 		#print(saveToFile)
 
-	with h5py.File("braintumors_1/{}".format(file), 'r') as f: #open .mat file to save to new .csv file corresponding to that .mat file
-		#print(type(f['cjdata/image'].value[0][0]))
-		#print(type(int(f['cjdata/image'].value[0][0])))
-		np.savetxt(saveToFile, f['cjdata/image'].value, delimiter=',') #For formatting: https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.savetxt.html
-		print("Wrote {0} to {1}".format(file, saveToFile))
+	try:
+		with h5py.File("braintumors_1/{}".format(file), 'r') as f: #open .mat file to save to new .csv file corresponding to that .mat file
+			#print(type(f['cjdata/image'].value[0][0]))
+			#print(type(int(f['cjdata/image'].value[0][0])))
+			print(saveToFile)
+			#print(f['cjdata/image'].value)
+			try:
+			   np.savetxt(saveToFile, f['cjdata/image'].value, delimiter=',') #For formatting: https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.savetxt.html
+			except ValueError as e:
+			   print('Save failed! {}'.format(str(e)))
+			   raise SystemError
+			except AttributeError as e:
+			   print('Save failed! {}'.format(str(e)))
+			   raise SystemError
+			print("Wrote {0} to {1}".format(file, saveToFile))
+	except:
+		print("h5py failed!")
+		raise
 
